394d4cb44cf7167cd331050f12d7fc4249ed77eb
Ximeen <gabriximenes100@gmail.com>
2022-11-29 19:52:09 -0300

    :bug: Correction of crew data

--- src/app/dashboard/tripulacao/CreateModal.tsx
+++ src/app/dashboard/tripulacao/CreateModal.tsx
@@ -11,7 +11,7 @@ export function ModalContent(Props: PropsChild){
     return(
       <Dialog.Portal>
         <Dialog.Overlay className='bg-black/50 fixed inset-0'/>
-        <Dialog.Content className='z-50 fixed bg-gray-100 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl lg:w-[80%] w-[80%]  shadow-lg shadow-black/25'>
+        <Dialog.Content className='z-50 fixed bg-gray-100 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl lg:w-[80%] w-[80%] lg:h-4/5 shadow-lg shadow-black/25'>
             {Props.children}
         </Dialog.Content>
       </Dialog.Portal>
