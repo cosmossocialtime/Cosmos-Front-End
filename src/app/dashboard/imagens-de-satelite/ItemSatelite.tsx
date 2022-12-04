d6336339778915aecf28d14ca911cface92d6d89
Ximeen <gabriximenes100@gmail.com>
2022-12-04 11:25:23 -0300

    :package: switch remote

--- src/app/dashboard/imagens-de-satelite/ItemSatelite.tsx
+++ src/app/dashboard/imagens-de-satelite/ItemSatelite.tsx
@@ -1,31 +1,36 @@
-// "use client";
-
 import Image from "next/image";
 import * as Dialog from "@radix-ui/react-dialog";
-
-
-// import * as Dialog from "@radix-ui/react-dialog";
+import { HTMLAttributes } from "react";
 
 interface ItemSateliteProps {
   sateliteImg: string;
   imgAlt: string;
   sateliteName: string;
   handleModal?: () => void;
+  className: string;
 }
 
 export function ItemSatelite({
   sateliteImg,
   imgAlt,
   sateliteName,
+  className,
 }: ItemSateliteProps) {
   return (
-    <Dialog.Trigger>
-      <div
-        className="bg-zinc-200/5 w-fit h-fit rounded-lg py-3 flex flex-col items-center cursor-pointer backdrop-blur-sm">
-        <img src={sateliteImg} alt={imgAlt} className="mx-auto" />
-        <p className="w-[130px] text-center font-bold text-white">
-          {sateliteName}
-        </p>
+    <Dialog.Trigger className={`${className}`}>
+      <div className="w-36 h-36">
+        <div className="bg-zinc-200/5 w-full h-full rounded-lg py-3 flex flex-col items-center justify-center cursor-pointer backdrop-blur-sm">
+          <Image
+            src={sateliteImg}
+            alt={imgAlt}
+            className="mx-auto"
+            width={52}
+            height={52}
+          />
+          <p className="w-[130px] text-center font-bold text-white">
+            {sateliteName}
+          </p>
+        </div>
       </div>
     </Dialog.Trigger>
   );
--- src/app/dashboard/imagens-de-satelite/page.tsx
+++ src/app/dashboard/imagens-de-satelite/page.tsx
@@ -5,85 +5,97 @@ import { ModalSatelite } from "./modalSatelite";
 import { ItemSatelite } from "./ItemSatelite";
 
 const SatelitesPage = () => {
-
-
   return (
     <>
       <Dialog.Root>
-        <ModalSatelite/>
+        <ModalSatelite />
 
-        <div className="px-20 w-full h-screen overflow-y-auto bg-bgsatelites bg-cover bg-center">
-          <h3 className="text-white">
-            Clique sobre a Estrela e os planetas para conhecer mais sobre a
+        <div className="flex flex-col items-center justify-center gap-9 px-20 pt-4 w-full h-screen lg:overflow-y-hidden bg-bgsatelites bg-cover bg-center">
+          <h3 className="text-white text-2xl font-semibold text-center">
+            Clique sobre a Estrela e os planetas para conhecer mais sobre a{" "}
+            <br />
             instituição que você irá mentorar
           </h3>
 
-          <div className="flex flex-wrap gap-[15%] gap-y-3 justify-center">
-            <ItemSatelite
-              sateliteImg="/images/satelites/recursos.png"
-              imgAlt={"planeta roxo"}
-              sateliteName={"Captação de Recursos"}
-            />
+          <div className="lg:overflow-hidden">
+            <div className="grid grid-cols-4 lg:grid-cols-8 lg:grid-rows-6 gap-2 justify-center">
+              <ItemSatelite
+                sateliteImg="/images/satelites/recursos.png"
+                imgAlt={"planeta roxo"}
+                sateliteName={"Captação de Recursos"}
+                className={"lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/financas.png"
-              imgAlt={"planeta azul"}
-              sateliteName={"Finanças"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/financas.png"
+                imgAlt={"planeta azul"}
+                sateliteName={"Finanças"}
+                className={"lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-1 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/juridico.png"
-              imgAlt={"planeta vermelho"}
-              sateliteName={"Jurídico"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/juridico.png"
+                imgAlt={"planeta vermelho"}
+                sateliteName={"Jurídico"}
+                className={"lg:col-start-5 lg:col-end-6 lg:row-start-1 lg:row-end-1 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/pessoas.png"
-              imgAlt={"planeta amarelo"}
-              sateliteName={"Gestão de Pessoas"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/pessoas.png"
+                imgAlt={"planeta amarelo"}
+                sateliteName={"Gestão de Pessoas"}
+                className={"lg:col-start-7 lg:col-end-8 lg:row-start-1 lg:row-end-3 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/marketing.png"
-              imgAlt={"planeta rosa"}
-              sateliteName={"Marketing"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/marketing.png"
+                imgAlt={"planeta rosa"}
+                sateliteName={"Marketing"}
+                className={"lg:col-start-2 lg:col-end-2 lg:row-start-3 lg:row-end-3 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/instituto.png"
-              imgAlt={"estrela branca"}
-              sateliteName={"Instituto Dorina Nowill"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/instituto.png"
+                imgAlt={"estrela branca"}
+                sateliteName={"Instituto Dorina Nowill"}
+                className={"lg:col-start-4 lg:col-end-4 lg:row-start-3 lg:row-end-4 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/impacto.png"
-              imgAlt={"planeta verde e azul"}
-              sateliteName={"Avaliação de Impacto"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/impacto.png"
+                imgAlt={"planeta verde e azul"}
+                sateliteName={"Avaliação de Impacto"}
+                className={"lg:col-start-6 lg:col-end-6 lg:row-start-3 lg:row-end-3 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/projetos.png"
-              imgAlt={"planeta laranja"}
-              sateliteName={"Gestão de Projetos"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/projetos.png"
+                imgAlt={"planeta laranja"}
+                sateliteName={"Gestão de Projetos"}
+                className={"lg:col-start-1 lg:col-end-1 lg:row-start-4 lg:row-end-6 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/sustentabilidade.png"
-              imgAlt={"planeta verde claro"}
-              sateliteName={"Sustentabilidade"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/sustentabilidade.png"
+                imgAlt={"planeta verde claro"}
+                sateliteName={"Sustentabilidade"}
+                className={"lg:col-start-3 lg:col-end-4 lg:row-start-5 lg:row-end-6 "}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/estrategia.png"
-              imgAlt={"planeta azul com roxo"}
-              sateliteName={"Estratégia"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/estrategia.png"
+                imgAlt={"planeta azul com roxo"}
+                sateliteName={"Estratégia"}
+                className={"lg:col-start-5 lg:col-end-5 lg:row-start-5 lg:row-end-5"}
+              />
 
-            <ItemSatelite
-              sateliteImg="/images/satelites/lideranca.png"
-              imgAlt={"planeta azul"}
-              sateliteName={"Liderança"}
-            />
+              <ItemSatelite
+                sateliteImg="/images/satelites/lideranca.png"
+                imgAlt={"planeta azul"}
+                sateliteName={"Liderança"}
+                className={"lg:col-start-7 lg:col-end-7 lg:row-start-4 lg:row-end-6 "}
+              />
+            </div>
           </div>
         </div>
       </Dialog.Root>
