994cb881d08e7780b5eeb1b3cf93741fa9f6e1cf
ximeen <gabriximenes100@gmail.com>
2023-04-03 20:45:53 -0300

    update form login and register

--- src/components/Cadastro/RegisterForm.tsx
+++ src/components/Cadastro/RegisterForm.tsx
@@ -1,10 +1,11 @@
+import * as HoverCard from '@radix-ui/react-hover-card';
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { z } from "zod"
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
-import { Eye, EyeClosed, Question } from "phosphor-react";
+import { Check, Eye, EyeClosed, Question } from "phosphor-react";
 import Link from "next/link";
-import * as HoverCard from '@radix-ui/react-hover-card';
 
 const schema = z.object({
   name: z.string(),
@@ -20,6 +21,8 @@ export function UserRegisterForm() {
   const [isSubmiting, setIsSubmiting] = useState(false)
   const [showPassword, setShowPassword] = useState(false);
   const [showPassword1, setShowPassword1] = useState(false);
+  const [acceptTerms, setAcceptTerms] = useState(false)
+  const [messageErrorPassword, setMessageErrorPassword] = useState("")
 
   const { register, handleSubmit } = useForm<formProps>()
   function handleForm(data: formProps) {
@@ -27,28 +30,29 @@ export function UserRegisterForm() {
 
     if (data.password.length <= 8) {
       setIsSubmiting(false)
-      return toast.error("A senha deve ser maior que 8 caracteres")
+      return setMessageErrorPassword("A senha deve ser maior que 8 caracteres")
     }
     if (!/[A-Z]/.test(data.password)) {
       setIsSubmiting(false)
-      return toast.error("A senha deve conter ao menos uma letra maiscula")
+      return setMessageErrorPassword("A senha deve conter ao menos uma letra maiscula")
     }
     if (!/\d/.test(data.password)) {
       setIsSubmiting(false)
-      return toast.error("A senha deve ao menos um número")
+      return setMessageErrorPassword("A senha deve ao menos um número")
     }
 
     if (data.password !== data.confirmPassword) {
       setIsSubmiting(false)
-      return toast.error("As senhas devem ser iguais")
+      return setMessageErrorPassword("As senhas devem ser iguais")
     }
-    return (toast.success("Criado com sucesso"), console.log(data));
+    setMessageErrorPassword("")
+    return (toast.success("Criado com sucesso"), console.log(data, acceptTerms));
 
   }
 
   return (
     <main className="flex flex-col w-1/2 gap-2 justify-center items-center">
-      <h1 className="text-3xl text-[#68459E]">Cadastro para voluntariado</h1>
+      <h1 className="text-3xl text-purple-700">Cadastro para voluntariado</h1>
       <form onSubmit={handleSubmit(handleForm)} className="w-1/2 flex flex-col gap-2 mt-4">
         <div className="flex flex-col w-full max-w-md gap-2">
           <label htmlFor="Nome">Nome completo</label>
@@ -57,7 +61,7 @@ export function UserRegisterForm() {
             required
             id="Nome"
             placeholder="Nome e sobrenome"
-            className="border-solid border border-gray-400 rounded-md p-2 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600"
+            className="border-solid border border-gray-400 rounded-md p-2 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600 valid:border-green-500"
           />
         </div>
 
@@ -68,7 +72,7 @@ export function UserRegisterForm() {
             required
             id="apelido"
             placeholder="Como gostaria de ser chamado(a)"
-            className="border-solid border border-gray-400 rounded-md p-2 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600"
+            className="border-solid border border-gray-400 rounded-md p-2 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600 valid:border-green-500"
           />
         </div>
 
@@ -80,7 +84,7 @@ export function UserRegisterForm() {
             id="email"
             type="email"
             placeholder="nome@email.com.br"
-            className="border-solid border border-gray-400 rounded-md p-2 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600"
+            className="border-solid border border-gray-400 rounded-md p-2 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500  invalid:border-pink-600 valid:border-green-500"
           />
         </div>
 
@@ -106,7 +110,7 @@ export function UserRegisterForm() {
             minLength={8}
             type={showPassword ? "text" : "password"}
             placeholder="Digite sua senha aqui"
-            className="w-full border-solid border border-gray-400 rounded-md p-2 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600 peer-[]:"
+            className="w-full border-solid border border-gray-400 rounded-md p-2 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600 "
           />
           <button
             className="button-show-password absolute top-3 right-2"
@@ -116,7 +120,7 @@ export function UserRegisterForm() {
             {showPassword ? <EyeClosed size={20} /> : <Eye size={20} />}
           </button>
         </div>
-        <span>A senha deve ter no mínimo 8 caracteres, com pelo menos uma letra maiúscula e um número.</span>
+        <span className="text-rose-600 text-sm">{messageErrorPassword}</span>
 
         <label htmlFor="confirm-password">Confirme a senha</label>
         <div className="flex justify-between group w-full max-w-md gap-2 relative">
@@ -126,7 +130,7 @@ export function UserRegisterForm() {
             id="confirm-password"
             type={showPassword1 ? "text" : "password"}
             placeholder="Digite sua senha aqui"
-            className="w-full border-solid border p-2 rounded-md focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600 "
+            className="w-full border-solid border border-gray-400 p-2 rounded-md focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 invalid:border-pink-600"
           />
           <button
             className="button-show-password absolute top-3 right-2"
@@ -137,6 +141,30 @@ export function UserRegisterForm() {
           </button>
         </div>
 
+        <div className='flex gap-2 my-4'>
+          <Checkbox.Root
+            className='bg-white w-6 h-6 border-2 border-solid border-[#A2ABCC] rounded flex items-center justify-center'
+            id='checkbox'
+            required
+            checked={acceptTerms}
+            onCheckedChange={
+              (checked) => {
+                if (checked === true) {
+                  setAcceptTerms(true)
+                } else {
+                  setAcceptTerms(false)
+                }
+              }
+            }
+          >
+            <Checkbox.Indicator >
+              <Check size={32} className="p-1 text-green-500 font-bold" />
+            </Checkbox.Indicator>
+          </Checkbox.Root>
+          <label htmlFor="checkbox">
+            <span className='text-[#727CA3]'>Aceito o <strong className='text-[#0890F7] font-semibold'>termo de tratamento de dados</strong></span>
+          </label>
+        </div>
         <button type="submit" className="cBtn w-full  max-w-md mt-2" disabled={isSubmiting}>
           Criar Conta
         </button>
