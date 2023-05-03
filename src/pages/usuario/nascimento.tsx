5fd39dee5b8fab3cbc94f175c1c87383021465ea
ximeen <gabriximenes100@gmail.com>
2023-05-03 20:43:12 -0300

    :up: Update domain for ngrok

--- src/pages/usuario/nascimento.tsx
+++ src/pages/usuario/nascimento.tsx
@@ -1,5 +1,4 @@
-import * as Select from "@radix-ui/react-select"
-import InputSelect from "../../components/InputSelect";
+
 import { BackButton } from "../../components/Welcome/BackButton";
 
 export default function Nascimento() {
@@ -13,14 +12,6 @@ export default function Nascimento() {
             className="text-zinc-50 text-2xl font-semibold">Nos conte o dia em que você chegou à Terra</h1>
           <span
             className="text-zinc-50 text-xl font-extralight">Informe aqui sua data de nascimento ;)</span>
-
-
-          <InputSelect>
-            <Select.Trigger className="bg-zinc-50 w-full">
-              <Select.Value />
-              <Select.Icon />
-            </Select.Trigger>
-          </InputSelect>
         </form>
       </main>
     </>
