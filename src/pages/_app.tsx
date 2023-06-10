a75fd8b022e4c568abb03dda2f10b0f346a463d7
ximeen <gabriximenes100@gmail.com>
2023-06-10 13:22:20 -0300

    :up: implement nprogress for smooth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "../styles/GlobalStyle";
 import { AuthProvider } from "../context/AuthProvider";
-import { ProtectedLayout } from "../components/ProtectedLayout";
+import Router from "next/router";
+import Nprogress from "nprogress"
 
 import "../lib/dayjs"
 import "../styles/mainTailwind.css";
 import "./dashboard/satellite-images/planets.css"
 import "keen-slider/keen-slider.min.css";
+
+Router.events.on("routeChangeStart", (url) => {
+  Nprogress.start()
+})
+Router.events.on("routeChangeComplete", () => {
+  Nprogress.done()
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
