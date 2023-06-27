2978b935eb9c583e66280a283671bfc83c403070
ximeen <gabriximenes100@gmail.com>
2023-06-27 18:47:51 -0300

    :bug: correction bug deploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,15 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600;800;900&display=swap"
             rel="stylesheet"
@@ -17,6 +21,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
