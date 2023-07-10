6e387b5161baec8a7740dbcc0dbd2ab174578fe4
erickhenri <herick.henri16@gmail.com>
2023-07-10 08:11:04 -0300

    create loading page

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-zinc-900 text-zinc-50">
+      <h1 className="text-lg">Carregando...</h1>
+    </div>
+  )
+}
