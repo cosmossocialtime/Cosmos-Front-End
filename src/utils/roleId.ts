a5c7e638b6d968c1c517e6969abc4fd224243ba7
erickhenri <herick.henri16@gmail.com>
2023-07-27 13:19:17 -0300

    create roleId infos

---
+++ src/utils/roleId.ts
@@ -0,0 +1,17 @@
+export const roleId = [
+  {
+    roleId: 1,
+    role: 'specialist',
+    roleName: 'Especialista',
+  },
+  {
+    roleId: 2,
+    role: 'pilot',
+    roleName: 'Piloto',
+  },
+  {
+    roleId: 3,
+    role: 'leader',
+    roleName: 'Comandante',
+  },
+]
