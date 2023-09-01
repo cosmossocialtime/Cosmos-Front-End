9da5f0729cd07083999e3423febc3283c2419ed5
erickhenri <herick.henri16@gmail.com>
2023-09-01 14:22:01 -0300

    update mentorship

--- src/pages/user/painel/index.tsx
+++ src/pages/user/painel/index.tsx
@@ -5,10 +5,21 @@ import AdventureArea from '../../../components/main-painel/painel/AdventureArea'
 import PerfilArea from '../../../components/main-painel/painel/PerfilArea'
 import AchievementsArea from '../../../components/main-painel/painel/AchievementsArea'
 import { Loading } from '../../../components/Loading'
-import { useDashboard } from '../../../hooks/useDashboard'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '../../../services/api'
+import { DashboardProps } from '../../../types/dashboard'
 
 export default function Painel() {
-  const { dashboard } = useDashboard()
+  async function getDashboard() {
+    const response = await api.get<DashboardProps>('/dashboard')
+
+    return response.data
+  }
+
+  const { data: dashboard } = useQuery({
+    queryKey: ['dashboard'],
+    queryFn: getDashboard,
+  })
 
   if (!dashboard) {
     return <Loading />
@@ -17,9 +28,7 @@ export default function Painel() {
   const achievements = dashboard.achievements
   const user = dashboard.user
   const programs = dashboard.programs
-  const mentorships = dashboard.currentMentorship
-    ? [dashboard.currentMentorship]
-    : []
+  const mentorships = dashboard.currentMentorships
 
   return (
     <div className="flex h-screen max-w-[100vw] flex-col">
